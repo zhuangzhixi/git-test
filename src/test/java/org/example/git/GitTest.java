5c1c16da973190e767cbd27f89e2558ad77c4b39
zhuangzhixi <2018418308@qq.com>
2022-11-13 14:18:31 +0800

    2

--- src/test/java/org/example/git/GitTest.java
+++ src/test/java/org/example/git/GitTest.java
@@ -3,5 +3,6 @@ package org.example.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
